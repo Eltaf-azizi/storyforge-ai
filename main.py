a534fc5c0ba04e125abee0e35f8bef4229ec6ea9
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-27 23:46:20 +0500

    working on the read root

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import staticfiles
+from fastapi.staticfiles import staticFiles
 from api.routes import router
 
 
@@ -10,4 +10,18 @@ app = FastAPI(title="AI-Powered Story Generator API")
 app.include_router(router)
 
 
-app.mount("/")
+app.mount("/static", staticFiles(directory="static", name="static"))
+
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the AI-Powered Story Generator API"}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+
