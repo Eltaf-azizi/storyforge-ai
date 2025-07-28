4e7307a523f7a8cbb1e87e97e33dae96a7c7c5f0
Eltaf-azizi <iltfhuss@gmail.com>
2025-07-28 22:35:32 +0500

    working to generate ai story

--- services/story_service.py
+++ services/story_service.py
@@ -12,3 +12,19 @@ load_dotenv()
 
 # get api key
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+client = openai.OpenAI(api_key = OPENAI_API_KEY)
+
+
+
+def generate_ai_story(genre: str, theme: str, length: str):
+    prompt = f"write a {length} {genre} story about {theme}. Make it engaging and creative"
+
+
+    response = client.chat.completions.create(
+        model="gpt-turbo-3.5",
+        messages=[
+            {"role": "system", "content": "you are a creative storyteller"}
+        ]
+    )
